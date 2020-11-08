4c76b87b09edbff8a1d75afee8d9a1650560602c
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2020-11-08 02:54:58 -0300

    Finalizada a persistencia e manipulação dos dados

--- src/database/readdata.js
+++ src/database/readdata.js
@@ -1,13 +1,100 @@
 const db = require("./db");
 
 module.exports.read = (callback) => {
-    query = `SELECT zonasorocaba, tipodeagua, COUNT(tipodeagua), AVG(cheirodaagua), AVG(cordaagua), aguanomomento, COUNT(aguanomomento) 
+    query = `SELECT zonasorocaba, tipodeagua, COUNT(tipodeagua) as contaagua, AVG(cheirodaagua) as mediacheiro, AVG(cordaagua) as mediacor, aguanomomento, COUNT(aguanomomento) as countaguamomento
     FROM results GROUP BY zonasorocaba, tipodeagua, aguanomomento`
 
     db.all(query, (err, rows) => {
+        
         if (err) {
             console.error(err.message)
         }
-        callback(rows)
+
+        var zonas = [
+            {  
+                name: "norte",
+                aguaencanada: 0,
+                aguapoco: 0,
+                mediacheiro: 0,
+                mediacor: 0,
+                aguanomomentosim: 0,
+                aguanomomentonao: 0
+            },
+            {
+                name: "sul",
+                aguaencanada: 0,
+                aguapoco: 0,
+                mediacheiro: 0,
+                mediacor: 0,
+                aguanomomentosim: 0,
+                aguanomomentonao: 0
+            },
+            {
+                name: "centro",
+                aguaencanada: 0,
+                aguapoco: 0,
+                mediacheiro: 0,
+                mediacor: 0,
+                aguanomomentosim: 0,
+                aguanomomentonao: 0
+
+            },
+            {
+                name: "leste",
+                aguaencanada: 0,
+                aguapoco: 0,
+                mediacheiro: 0,
+                mediacor: 0,
+                aguanomomentosim: 0,
+                aguanomomentonao: 0
+            },
+            {
+                name: "oeste",
+                aguaencanada: 0,
+                aguapoco: 0,
+                mediacheiro: 0,
+                mediacor: 0,
+                aguanomomentosim: 0,
+                aguanomomentonao: 0
+
+            }
+        ]
+
+        rows.forEach(dataelement => {
+
+            var index = zonas.findIndex(zona => zona.name == dataelement.zonasorocaba)
+
+            if (dataelement.zonasorocaba == zonas[index].name) {
+                if (dataelement.tipoagua == "encanada") {
+    
+                    zonas[index].aguaencanada += dataelement.contaagua
+    
+                    if (dataelement.aguanomomento == "sim") {
+                        zonas[index].aguanomomentosim += dataelement.countaguamomento
+                    } else {
+                        zonas[index].aguanomomentonao += dataelement.countaguamomento
+                    }
+                } else {
+    
+                    zonas[index].aguapoco += dataelement.contaagua
+    
+                    if (dataelement.aguanomomento == "sim") {
+    
+                        zonas[index].aguanomomentosim += dataelement.countaguamomento
+                    } else {
+    
+                        zonas[index].aguanomomentonao += dataelement.countaguamomento
+                    }
+                }
+    
+                zonas[index].mediacheiro += dataelement.mediacheiro
+                zonas[index].mediacor += dataelement.mediacor
+    
+            }
+
+        })
+
+        callback(zonas)
+
     })
 }
--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,7 @@
 // Importando o DB.
 const db = require("./database/db");
-const includeData = require("./database/includedata");
 const data = require("./database/readdata");
+const includeData = require("./database/includedata");
 
 function research(req, res) {
 
@@ -13,18 +13,16 @@ function saveResearch(req, res) {
 	const data = req.body;
 
 	includeData(db, data);
-	console.log(data);
+	// console.log(data);
 
 	res.redirect("/results")
 }
 
 function results(req, res) {
-
 	data.read((data) => {
-		console.log(data)
 		return res.render("results.html", { data })
-	})
-}
+	});
+};
 
 
 module.exports = {
